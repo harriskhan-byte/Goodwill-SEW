d72016461c32d0d050fb5394cb47ad0db314489f
Austin Bird <va-abird@goodwillsew.com>
2025-07-18 20:49:56 +0000

    feat: DIM_PurchaseOrder final

--- DIM_PurchaseOrder.sql
+++ DIM_PurchaseOrder.sql
@@ -1,11 +1,28 @@
--- CREATE VIEW DIM_PurchaseOrder AS
+-- CREATE VIEW ProcurementReporting.DIM_PurchaseOrder AS
+
+/*  DIM SQL Script Creation - Purchase Order
+    Only select statement - view creation will be done by client
+    Name: DIM_PurchaseOrder
+    Location: ProcurementReporting
+
+    Fields:
+      PurchaseOrderKey        (PurchTable.RecId)
+      PurchaseOrderNumber     (PurchTable.PurchId)
+      CreatedDate             (PurchTable.CreatedDateTime)
+      POStatus                (PurchTable.PurchStatus)
+      RequestedReceiptDate    (PurchTable.ReceiptDateRequested)
+      DeliveryAddress         (LogisticsPostalAddress.Address)
+      AttentionInformation    from address???
+*/
+
 SELECT
-    pt.RECID AS PurchaseOrderKey,
-    pt.PurchId AS PurchaseOrderNumber,
-    pt.CREATEDDATETIME AS CreatedDate,
-    pt.PURCHSTATUS AS POStatus,
-    pt.REQUESTEDSHIPDATE AS RequestedReceiptDate,
-    lpa.ADDRESS AS DeliveryAddress
-    --lpa. AS AttentionInformation -- Could not find this
+    pt.RECID AS PurchaseOrderKey,                  -- Surrogate key for purchase order
+    pt.PurchId AS PurchaseOrderNumber,             -- Purchase order number
+    pt.CREATEDDATETIME AS CreatedDate,             -- Purchase order creation date/time
+    pt.PURCHSTATUS AS POStatus,                    -- Purchase order status
+    pt.REQUESTEDSHIPDATE AS RequestedReceiptDate,  -- Requested receipt date
+    lpa.ADDRESS AS DeliveryAddress                 -- Delivery address from LogisticsPostalAddress
+    -- lpa.[???] AS AttentionInformation           -- AttentionInformation field not found in address table
 FROM PURCHTABLE pt
-LEFT JOIN LOGISTICSPOSTALADDRESS lpa ON pt.DeliveryPostalAddress = lpa.RecId;
+LEFT JOIN LOGISTICSPOSTALADDRESS lpa 
+    ON pt.DeliveryPostalAddress = lpa.RecId;
