dda5c521dc3de91ce94ed9bfc115506d89946985
Austin Bird <va-abird@goodwillsew.com>
2025-07-18 20:47:03 +0000

    feat: DIM_ProcurementCategory final

--- DIM_ProcurementCategory.sql
+++ DIM_ProcurementCategory.sql
@@ -1,6 +1,20 @@
+-- CREATE VIEW ProcurementReporting.DIM_ProcurementCategory AS
+
+/*  DIM SQL Script Creation - Procurement Category
+    Only select statement - view creation will be done by client
+    Name: DIM_ProcurementCategory
+    Location: ProcurementReporting
+
+    Fields:
+      ProcurementCategoryKey         (EcoResCategory.RecId)
+      ProcurementCategory            (EcoResCategory.Name)
+      ProcurementCategoryDescription (EcoResCategoryTranslation.Description)
+*/
+
 SELECT 
-	ec.RECID AS ProcurmentCategoryKey,
-	ec.Name AS ProcurementCategory,
-	ect.DESCRIPTION AS ProcurementCategoryDescription
+    ec.RECID AS ProcurementCategoryKey,                -- Surrogate key for procurement category
+    ec.NAME AS ProcurementCategory,                    -- Procurement category name
+    ect.DESCRIPTION AS ProcurementCategoryDescription  -- Procurement category description (translation)
 FROM ECORESCATEGORY ec
-LEFT JOIN ECORESCATEGORYTRANSLATION ect on ect.CATEGORY = ec.RECID
+LEFT JOIN ECORESCATEGORYTRANSLATION ect 
+    ON ect.CATEGORY = ec.RECID;
