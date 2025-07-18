47364d31ddd9e82b051b4ce38e982cbee98ee8c4
Austin Bird <va-abird@goodwillsew.com>
2025-07-18 20:51:24 +0000

    feat: DIM_VendorInvoice final

--- DIM_VendorInvoice.sql
+++ DIM_VendorInvoice.sql
@@ -1,6 +1,20 @@
+-- CREATE VIEW ProcurementReporting.DIM_VendorInvoice AS
+
+/*  DIM SQL Script Creation - Vendor Invoices
+    Only select statement - view creation will be done by client
+    Name: DIM_VendorInvoice
+    Location: ProcurementReporting
+
+    Fields:
+      VendorInvoiceKey   (VendInvoiceJour.RecId)
+      InvoiceId          (VendInvoiceJour.InvoiceId)
+      InvoiceDate        (VendInvoiceJour.DocumentDate)
+      PurchaseType       (VendInvoiceJour.PurchaseType)
+*/
+
 SELECT
-    vij.RecId AS VendorInvoiceKey, -- using this from vend invoice jour rather than vend
-    vij.InvoiceId AS InvoiceId,
-    vij.DocumentDate AS InvoiceDate,
-    vij.PurchaseType AS PurchaseType
+    vij.RECID AS VendorInvoiceKey,         -- Surrogate key for vendor invoice (RecId)
+    vij.INVOICEID AS InvoiceId,            -- Vendor invoice number
+    vij.DOCUMENTDATE AS InvoiceDate,       -- Invoice document date
+    vij.PURCHASETYPE AS PurchaseType       -- Purchase type
 FROM VendInvoiceJour vij;
