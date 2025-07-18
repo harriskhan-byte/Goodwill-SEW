73f9ff9c337e4f85a027957c10d40d6b1c72b0c7
Austin Bird <va-abird@goodwillsew.com>
2025-07-18 20:43:09 +0000

    feat: DIM_Company final

--- DIM_Company.sql
+++ DIM_Company.sql
@@ -1,10 +1,24 @@
+-- CREATE VIEW ProcurementReporting.DIM_Company AS
+
+/*  DIM SQL Script Creation - Company
+    Only select statement - view creation will be done by client
+    Name: DIM_Company
+    Location: ProcurementReporting
+
+    Fields:
+      CompanyKey   (CompanyInfo.DataArea)
+      CompanyCode  (CompanyInfo.DataArea)
+      CompanyName  (DirPartyTable.Name)
+*/
+
 SELECT 
-	dpt.DATAAREA AS CompanyKey,
-	dpt.DATAAREA AS CompanyCode, 
-	dpt.NAME AS CompanyName
+    dpt.DATAAREA AS CompanyKey,      -- Surrogate key for company (DataArea)
+    dpt.DATAAREA AS CompanyCode,     -- Company code (DataArea)
+    dpt.NAME     AS CompanyName      -- Company name from DirPartyTable
 FROM DIRPARTYTABLE dpt
+-- Filter to only company records (CompanyInfo)
 WHERE dpt.INSTANCERELATIONTYPE = (
     SELECT TOP 1 RECID
     FROM TABLEIDTABLE
     WHERE NAME = 'CompanyInfo'
-)
+);
